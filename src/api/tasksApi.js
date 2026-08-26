6c33af84145c9f4d53a46ceae43ae2b0f160d7ce
mohamed magdy <mohamedmagdy5061@gmail.com>
2026-08-26 13:25:27 +0300

    update api base url

--- src/api/tasksApi.js
+++ src/api/tasksApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:4000',
+  baseURL: 'https://kanban-json-server-api.vercel.app',
 });
 
 export const fetchTasks = async () => {
